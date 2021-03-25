dac15508e0acd7bb1c4fea78e01b089589243d12
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-03-25 16:34:50 +0100

    stop objekt erzeugt und verwendet

--- nz/main.js
+++ nz/main.js
@@ -1,8 +1,21 @@
+let stop = {
+    nr: 10,
+    name: "Sandfly Bay",
+    lat: -45.896031,
+    lng: 170.647314,
+    user: "Ronald Christian Tabernig",
+    wikipedia: "https://en.m.wikipedia.org/wiki/Sandfly_Bay"
+};
+console.log(stop);
+console.log(stop.name);
+console.log(stop.lat);
+console.log(stop.lng);
+console.log(stop.wikipedia);
 console.log("Hello World");
 console.log(L);
 
 const map = L.map("map", {
-    center: [-45.896031, 170.647314],
+    center: [stop.lat,stop.lng],
     zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
@@ -10,11 +23,11 @@ const map = L.map("map", {
 });
 
 //Marker
-let mrk = L.marker([-45.896031, 170.647314]).addTo(map);
+let mrk = L.marker([stop.lat,stop.lng]).addTo(map);
 
 // Pop-Up
 mrk.bindPopup("Sandfly Bay").openPopup();
 
 // WMTS - Services
-console.log(document.querySelector("#map"))
+//console.log(document.querySelector("#map"))
 
