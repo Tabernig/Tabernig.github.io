31852436f7040800d9b3b3273c3e080f975c6e15
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-03-25 17:02:59 +0100

    popup mit template syntax

--- nz/main.js
+++ nz/main.js
@@ -26,7 +26,10 @@ const map = L.map("map", {
 let mrk = L.marker([stop.lat,stop.lng]).addTo(map);
 
 // Pop-Up
-mrk.bindPopup("Sandfly Bay").openPopup();
+mrk.bindPopup(`
+    <h4> Stop ${stop.nr}:${stop.name}</h4>
+    <p><i class ="fas fa-external-link-alt mr-3"></i><a href = "${stop.wikipedia}" >Read about stop in Wikipedia </a></p>
+`).openPopup();
 
 // WMTS - Services
 //console.log(document.querySelector("#map"))
