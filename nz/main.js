8ecead66212a54a4cb5617c4f349c93463e9a896
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-03-25 16:17:04 +0100

    Karte Erstellt

--- nz/main.js
+++ nz/main.js
@@ -1,6 +1,13 @@
 console.log("Hello World");
 console.log(L);
 
-const map = L.map("map");
+const map = L.map("map", {
+    center: [-45.896031, 170.647314],
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+// WMTS - Services
 console.log(document.querySelector("#map"))
 
