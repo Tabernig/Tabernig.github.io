9f9948bd62751cf226d50f24978045aadfd44de3
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-03-25 16:20:12 +0100

    marker hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -8,6 +8,10 @@ const map = L.map("map", {
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
+
+
+let mrk = L.marker([-45.896031, 170.647314]).addTo(map);
+
 // WMTS - Services
 console.log(document.querySelector("#map"))
 
