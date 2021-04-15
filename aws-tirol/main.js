e3432b1d62d8277fafe6e674dca1b4aca12a499f
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-04-15 17:19:22 +0200

    add local date

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -39,12 +39,13 @@ fetch(awsUrl)
                 station.geometry.coordinates[1],
                 station.geometry.coordinates[0]
             ]);
+            let formattedDate = new Date(station.properties.date);
             marker.bindPopup(`
             <h3>${station.properties.name}</h3>
             <ul> 
-                <li>Datum: ${station.properties.date}</li>
-                <li>Temperatur: ${station.properties.LT} C </li>
-                
+                <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+                <li>Temperatur: ${station.properties.LT}° C </li>
+
             </ul>
             
             
