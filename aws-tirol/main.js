53311e73407d4f236f44d04d0fa413478e64df75
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-04-15 17:37:00 +0200

    insert graphics

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -43,13 +43,15 @@ fetch(awsUrl)
             marker.bindPopup(`
             <h3>${station.properties.name}</h3>
             <ul> 
-                <li>Datum: ${formattedDate.toLocaleString("de")}</li>
-                <li>Temperatur: ${station.properties.LT}° C </li>
-
+                <li>Seehöhe: ${station.geometry.coordinates[2] || "?"} m</li>
+                <li>Datum: ${formattedDate.toLocaleString("de") || "?"}</li>
+                <li>Temperatur: ${station.properties.LT || "?"}° C </li>
+                <li>Luftfeuchtigkeit: ${station.properties.RH || "?"} % </li>
+                <li>Windgeschwindigkeit: ${station.properties.WG || "?"} km/h </li>
+                <li>Windrichtung: ${station.properties.WR || "?"} ° </li>
+                <li>Schneehöhe: ${station.properties.HS || "?"} cm </li>
             </ul>
-            
-            
-            
+            <a target = "_blank"href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.layerGroup}.png">Grafik</a>            
             `)
             marker.addTo(awsLayer);
             map.fitBounds(awsLayer.getBounds());
