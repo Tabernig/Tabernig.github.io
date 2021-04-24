bc7ea86ee6675119c8aa91a2c7a5b7597321d936
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-04-24 10:04:41 +0200

    rainviewer added

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -66,6 +66,29 @@ L.control.scale({
 }).addTo(map);
 
 
+
+//Rainviewer
+
+// Change default options
+L.control.rainviewer({ 
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
+
+
+
+
+
+
+
+
+
 //let hood = L.point()
 
 
@@ -201,13 +224,6 @@ fetch(awsUrl)
     });
     
     
-
-
-
-
-
-
-
 
 
 
