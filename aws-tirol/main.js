06e03395083cbe488e9e582a3cb471bfdfeb3d2e
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-04-28 10:41:59 +0200

    added WR to popup

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -165,7 +165,7 @@ fetch(awsUrl)
                 <li>Temperatur: ${station.properties.LT || "?"}° C </li>
                 <li>Luftfeuchtigkeit: ${station.properties.RH || "?"} % </li>
                 <li>Windgeschwindigkeit: ${station.properties.WG || "?"} km/h </li>
-                <li>Windrichtung: ${station.properties.WR || "?"} ° </li>
+                <li>Windrichtung: ${getDirection(station.properties.WR,DIRECTIONS) || "?"} ° </li>
                 <li>Schneehöhe: ${station.properties.HS || "?"} cm </li>
             </ul>
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
