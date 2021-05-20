ffd6c6f2e0ed35a15c1a4aecd6ad5e88a68e43e3
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-05-20 16:17:41 +0200

    popup added

--- tirol/main.js
+++ tirol/main.js
@@ -66,7 +66,14 @@ const drawTrack = (nr) => {
     gpxTrack.on("loaded", () => {
         console.log("loaded gpx.");
         map.fitBounds(gpxTrack.getBounds());
-    })
+        overlays.tracks.bindPopup(`
+        Segement: ${gpxTrack.get_name()} 
+        <hr>
+        Maximal Height: ${Math.round(gpxTrack.get_elevation_max())} m <br>
+        Minimal Height: ${Math.round(gpxTrack.get_elevation_min())} m <br>
+        Total Distance: ${Math.round(gpxTrack.get_distance())/1000} km`)
+    });
+   
 
 
 };
